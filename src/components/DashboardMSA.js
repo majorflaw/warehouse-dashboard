8af0cb79a40e7a314f20be5c29fa13124c76fcb0
majorflaw <buterezliviu@Buterezs-MacBook-Air.local>
2024-11-29 21:51:09 +0100

    New Layouts

--- src/components/DashboardMSA.js
+++ src/components/DashboardMSA.js
@@ -87,7 +87,7 @@ const DashboardMSA = () => {
   };
 
   return (
-    <div className="p-6">
+    <div className="p-6 bg-gray-900">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-8">
         <div className="bg-gray-800 rounded-lg shadow p-6">
           <div className="flex items-center">
